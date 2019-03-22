eeb0bedf939eaf07fb6ec76c402e35e717cde1bb
adminshan <2644586064@qq.com>
2019-03-22 16:08:14 +0800

    api

--- app/Http/Controllers/User/StartController.php
+++ app/Http/Controllers/User/StartController.php
@@ -102,6 +102,8 @@ class StartController extends Controller
             ];
         }else {
             $token = substr(md5(time().mt_rand(1,99999)),10,10);
+            setcookie('uid',$info->uid,time()+86400,'/','tactshan.com',false,true);
+            setcookie('token',$token,time()+86400,'/','tactshan.com',false,true);
             $redis_token='str:u:token:'.$info->uid;
             Redis::set($redis_token,$token);
             Redis::expire($redis_token,86400);
